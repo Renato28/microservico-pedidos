de001322848d55ad4b1746600cc5bce4b9953b87
Renato Nobrega <renatonobrega87@gmail.com>
2023-05-16 20:00:01 -0300

    implementando as classes Pedido, Item do Pedido e Enum Status

---
+++ pedidos/src/main/java/com/alurafood/pedidos/model/Pedido.java
@@ -0,0 +1,32 @@
+package com.alurafood.pedidos.model;
+
+import com.alurafood.pedidos.model.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "pedidos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "pedidos")
+    private List<ItemDoPedido> itens = new ArrayList<>();
+}
---
+++ pedidos/src/main/java/com/alurafood/pedidos/model/enums/Status.java
@@ -0,0 +1,12 @@
+package com.alurafood.pedidos.model.enums;
+
+public enum Status {
+    REALIZADO,
+    CANCELADO,
+    PAGO,
+    NAO_AUTORIZADO,
+    CONFIRMADO,
+    PRONTO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE;
+}
