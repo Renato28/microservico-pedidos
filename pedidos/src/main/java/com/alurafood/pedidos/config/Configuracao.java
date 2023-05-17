e8558942fa47496480f26b465aa7e833292391f3
Renato Nobrega <renatonobrega87@gmail.com>
2023-05-16 21:22:28 -0300

    Implementando a classe de Configuracao

---
+++ pedidos/src/main/java/com/alurafood/pedidos/config/Configuracao.java
@@ -0,0 +1,14 @@
+package com.alurafood.pedidos.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configuracao {
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+}
